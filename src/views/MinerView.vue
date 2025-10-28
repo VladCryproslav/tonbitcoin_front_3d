b35b55697801281a6b1c8fb28f165ab4a29a81a0
tonbitcoindev <tonbitcoindev@gmail.com>
2025-10-28 12:22:23 +0200

    UPD ASICs sale v1.23

--- src/views/MinerView.vue
+++ src/views/MinerView.vue
@@ -1430,7 +1430,7 @@ onUnmounted(() => {
   .shop-tabs {
     display: flex;
     width: auto;
-    margin: 0 0 0 15px;
+    margin: 0 0 0 30px;
     background: rgba(0, 0, 0, 0.5);
     border-radius: 20px;
     padding: 3px;
