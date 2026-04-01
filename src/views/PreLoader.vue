cf9ae60d593484ca7cf4dbdca195008006aa97e4
VladCryptoslav <vladcryptoslav@gmail.com>
2026-04-01 14:24:01 +0300

    Upd preloader

--- src/views/PreLoader.vue
+++ src/views/PreLoader.vue
@@ -18,7 +18,7 @@ const { tg } = useTelegram()
 const { t, locale } = useI18n()
 
 const TECH_MAINTENANCE = false; // Технічні роботи
-const TECH_INFOBOT = true;
+const TECH_INFOBOT = false;
 /** Блокуючий тех-екран: maintenance і/або окремий infobot-режим */
 const TECH_SCREEN = TECH_MAINTENANCE || TECH_INFOBOT
 const opacityLevels = ref([])
